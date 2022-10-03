6dbe54602cf5eca3ae22650c11b6756f813c8fbe
phalvesr <phalvesrnd@gmail.com>
2022-10-03 18:24:49 -0300

    remove variaveis nao usadas

--- src/main/java/JavaStream/config/Config.java
+++ src/main/java/JavaStream/config/Config.java
@@ -11,11 +11,7 @@ public final class Config {
         var inputStream = new FileInputStream("src/main/resources/application.properties");
         var properties = new Properties();
         properties.load(inputStream);
-
-        var url = properties.getProperty("database.h2.url");
-        var username = properties.getProperty("database.h2.username");
-        var password = properties.getProperty("database.h2.password");
-
+        
         return DriverManager.getConnection(properties.getProperty("database.h2.url"), properties.getProperty("database.h2.username"), properties.getProperty("database.h2.password"));
     }
 }
