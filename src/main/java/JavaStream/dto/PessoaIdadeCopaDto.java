3f4ea2f3ee65d225ed33a29bd7386cc2f0eb3be2
phalvesr <phalvesrnd@gmail.com>
2022-10-04 17:27:19 -0300

    fim merge

--- src/main/java/JavaStream/dto/PessoaIdadeCopaDto.java
+++ src/main/java/JavaStream/dto/PessoaIdadeCopaDto.java
@@ -3,17 +3,11 @@ package JavaStream.dto;
 import JavaStream.model.Pessoa;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-<<<<<<< HEAD
-
-@Getter
-@AllArgsConstructor
-=======
 import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @ToString
->>>>>>> master
 public class PessoaIdadeCopaDto {
     private Pessoa pessoa;
     private int idade;
